407d2807f3ea5eba68a7ca6afebcdaf6c589ed97
Andrea <andr3z45@gmail.com>
2022-01-19 23:24:52 +0100

    font

--- index.php
+++ index.php
@@ -46,8 +46,9 @@ $faqs = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
     <title>Google Faqs</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -81,7 +82,7 @@ $faqs = [
             <?php } ?>
         </div>
     </main>
-    
+
     <!-- FOOTER -->
 
     <footer>
